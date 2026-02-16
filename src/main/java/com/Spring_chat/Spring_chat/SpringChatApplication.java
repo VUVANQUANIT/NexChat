337adeb393a57a105b80197e98bd91befbd2b9ff
Vũ Văn Quân <quancn27@gmail.com>
2026-02-17 00:36:47 +0700

    Initial commit

---
+++ src/main/java/com/Spring_chat/Spring_chat/SpringChatApplication.java
@@ -0,0 +1,13 @@
+package com.Spring_chat.Spring_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Spring_chat/Spring_chat/SpringChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_chat.Spring_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
