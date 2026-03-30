8b5a956c933b3f03754e94a75e0c3586498f8bb9
Vũ Văn Quân <quancn27@gmail.com>
2026-03-30 22:40:41 +0700

    Fix bug endpoint controller

--- src/main/java/com/Spring_chat/Spring_chat/controller/friendship/FriendShipController.java
+++ src/main/java/com/Spring_chat/Spring_chat/controller/friendship/FriendShipController.java
@@ -36,7 +36,7 @@ public class FriendShipController {
     public ResponseEntity<ApiResponse<RejectFriendResponseDTO>> rejectRequestFriendShip(@PathVariable("id") Long id){
         return ResponseEntity.ok(friendShipService.rejectFriendShip(id));
     }
-    @PostMapping(" /friendships/{userId}/block")
+    @PostMapping("/friendships/{userId}/block")
     public ResponseEntity<ApiResponse<FriendResponseBlockDTO>> blockFriendship(@PathVariable("userId") Long id){
         return ResponseEntity.ok(friendShipService.blockFriendship(id));
     }
